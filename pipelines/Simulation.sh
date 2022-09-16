3a5f612633cfc4ea88ee74f27641b578040d7223
pratas <6882743+pratas@users.noreply.github.com>
2022-09-16 12:36:06 +0100

    simulation of evolution

---
+++ pipelines/Simulation.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+ITERATIONS=10000;
+RATE=0.001;
+#
+rm -f ALL.fa;
+#
+printf "\n" > DIV;
+#
+gto_genomic_gen_random_dna -n 2000 -s 7 \
+| gto_fasta_from_seq -n "Synthetic DNA" > IN.fa
+#
+for((x=1 ; x<=$ITERATIONS ; ++x));
+  do
+  #
+  printf "\rRunning iteration $x ...";
+  #
+  gto_fasta_mutate -s $x -e $RATE < IN.fa > TMP.fa
+  gto_fasta_to_seq < TMP.fa \
+  | gto_fasta_from_seq -l 70 -n "Synthetic DNA i=$x" > OUT.fa
+  cat OUT.fa DIV >> ALL.fa
+  cp OUT.fa INT.fa
+  done
+#
+rm -f TMP.fa
+#
+echo "";
