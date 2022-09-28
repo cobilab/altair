640c975cda113f90ebd0a6444dbb360c01629acd
pratas <6882743+pratas@users.noreply.github.com>
2022-09-28 09:48:07 +0100

    filter with altair average - much better

--- pipelines/SimProfileSars.sh
+++ pipelines/SimProfileSars.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 #
-FIL_DROP=0;
-#
-W1=2;
+W1=1;
 W2=10;
 W3=100;
 #
 cat $1 \
 | awk '{ print $1"\t"$2;}' \
-| gto_filter -w $W1 -t $2 -d $FIL_DROP > .NCDFiltered.txt
+| ./AltaiR average -v -c 2 -p -w $W1 > .NCDFiltered.txt
 #
 cat $1 \
 | awk '{ print $1"\t"$2;}' \
-| gto_filter -w $W2 -t $2 -d $FIL_DROP > .NCDFiltered2.txt
+| ./AltaiR average -v -c 2 -p -w $W2 > .NCDFiltered2.txt
 #
 cat $1 \
 | awk '{ print $1"\t"$2;}' \
-| gto_filter -w $W3 -t $2 -d $FIL_DROP > .NCDFiltered3.txt
+| ./AltaiR average -v -c 2 -p -w $W3 > .NCDFiltered3.txt
 #
 gnuplot << EOF
     reset
@@ -26,20 +24,20 @@ gnuplot << EOF
     set border 3 front ls 101
     set tics nomirror out scale 0.5
     set format '%g'
-    set size ratio 0.2
+    set size ratio 0.14
     set key out horiz center top
-    set yrange [$3:$4]
+    set yrange [$2:$3]
     set xrange [:]
     set xtics auto
-    set ytics auto
+    set ytics 0.002
     set grid
     set ylabel "NCD"
     set xlabel "Time"
     set border linewidth 1.5
     set style line 1 lc rgb '#322152' lt 1 lw 1 pt 5 ps 0.4 # --- blue
-    set style line 3 lc rgb '#009900' lt 1 lw 1.5 pt 6 ps 0.4 # --- green
-    set style line 2 lc rgb '#CC0000' lt 1 lw 1.5 pt 7 ps 0.4 # --- red
-    set xtics border in scale 0,0 nomirror rotate by -55  autojustify
+    set style line 2 lc rgb '#009900' lt 1 lw 1.5 pt 6 ps 0.4 # --- green
+    set style line 3 lc rgb '#CC0000' lt 1 lw 2 pt 7 ps 0.4 # --- red
+    #set xtics border in scale 0,0 nomirror rotate by -55  autojustify
     set xtics  norangelimit  font ",8"
     nth(countCol,labelCol,n) = ((int(column(countCol)) % n == 2500) ? stringcolumn(labelCol) : "")
     plot ".NCDFiltered.txt" using 1:2 title '  w=$W1' with lines ls 1, ".NCDFiltered2.txt" using 1:2 title '  w=$W2' with lines ls 2, ".NCDFiltered3.txt" using 1:2 title '  w=$W3' with lines ls 3
