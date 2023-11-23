cc83252ccf3186ddde3dcb9611fbb9bf229fbf74
pratas <6882743+pratas@users.noreply.github.com>
2023-11-23 14:55:42 +0000

    Generic histogram

---
+++ pipelines/GenericHistogram.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# 
+# ./GenericHistogram.sh sequences.fa 10000 20000
+#                         (FASTA)    (MIN) (MAX)
+#
+MIN=$2;
+MAX=$3;
+#
+cat $1 \
+| awk '$0 ~ ">" {print c; c=0;printf substr($0,2,10) "\t"; } $0 !~ ">" {c+=length($0);} END { print c; }' \
+| sed '/^$/d' \
+| awk '{print $2}' \
+| sort \
+| uniq -c \
+| awk '{print $1"\t"$2}' > histogram-data.txt;
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,12'
+    set output "Histogram.pdf"
+    set style line 101 lc rgb '#000000' lt 1 lw 2
+    set border 3 front ls 101
+    set tics nomirror out scale 0.75
+    set key fixed right top vertical Right noreverse noenhanced autotitle nobox
+    set style histogram clustered gap 1 title textcolor lt -1
+    set style data histograms
+    set xtics border in scale 0,0 nomirror #rotate by -60  autojustify
+    set yrange [:]
+    set xrange [$MIN:$MAX]
+    set xtics auto
+    set ytics auto
+    set grid
+    set ylabel "Frequency"
+    set xlabel "Sequence length"
+    set style line 4 lc rgb '#CC0000' lt 2 dashtype '---' lw 4 pt 5 ps 0.4 # --- red
+    set arrow from 29903, graph 0 to 29903, graph 1 nohead ls 4
+    plot "histogram-data.txt" u 2:1 w boxes lc rgb"blue" notitle
+EOF
+#
